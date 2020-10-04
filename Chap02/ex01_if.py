bbdaa4563c7e17da83214fef5c7e8edbfb88569f
DESKTOP-BB105KM\user <goodluck3586@gsm.hs.kr>
2020-10-04 15:54:04 +0900

    gg

---
+++ Chap02/ex01_if.py
@@ -0,0 +1,33 @@
+#region if ~ else문
+# num = int(input("정수를 입력하세요: "))
+
+# # 1. num이 짝수인지 검사
+# if num%2==0:
+#     print('짝수')
+# else:
+#     print('홀수')
+
+# # 2. num이 양수이고, 짝수인지 검사
+# if num>0:
+#     if num%2==0:
+#         print('짝수')
+#     else:
+#         print('홀수')
+# else:
+#     print('자연수를 입력하세요.')
+
+# 3. 버스카드 또는 현금
+card = True
+cash = False
+if card == True or cash == True:
+    print('버스 탑승 가능')
+else:
+    print('버스 탑승 불가')
+
+#endregion
+
+
+
+
+
+
